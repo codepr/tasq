466766fe184b49478ef3df4077b1283a59ec36a5
Andrea <a.g.baldan@gmail.com>
2020-07-17 19:06:45 +0200

    Add job unit tests

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
+import unittest
 from setuptools import setup
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
+
+def test_suite():
+    test_loader = unittest.TestLoader()
+    tests = test_loader.discover("tests", pattern="*_test.py")
+    return tests
+
+
 # Dependencies
-required = ['zmq', 'cloudpickle', 'redis', 'pika']
+required = ["zmq", "cloudpickle", "redis", "pika"]
 
 setup(
-    name='tasq',
-    version='1.1.6',
-    description='A simple task queue implementation leveraging zmq and a naive '
-                'implementation of the actor model to enqeue jobs on local or remote processes',
+    name="tasq",
+    version="1.2.1",
+    description="A simple task queue implementation leveraging zmq and a naive "
+    "implementation of the actor model to enqeue jobs on local or remote processes",
     long_description=readme,
-    author='Andrea Giacomo Baldan',
-    author_email='a.g.baldan@gmail.com',
-    packages=['tasq', 'tasq.actors', 'tasq.remote', 'tasq.cli'],
+    author="Andrea Giacomo Baldan",
+    author_email="a.g.baldan@gmail.com",
+    packages=["tasq", "tasq.actors", "tasq.remote", "tasq.cli"],
     install_requires=required,
-    scripts=['tq']
+    scripts=["tq"],
+    test_suite="setup.test_suite",
 )
---
+++ tests/job_test.py
@@ -0,0 +1,39 @@
+import time
+import unittest
+from tasq.job import Job, JobStatus, JobResult
+
+
+class TestJob(unittest.TestCase):
+    def test_job_init(self):
+        job = Job("job-1", lambda x: x + 1, 10)
+        self.assertEqual(job.job_id, "job-1")
+        self.assertEqual(job.status, JobStatus.PENDING)
+        self.assertEqual(job.delay, 0)
+
+    def test_job_create(self):
+        job = Job.create(lambda x: x + 1, name="job-1", x=10)
+        self.assertEqual(job.job_id, "job-1")
+        self.assertEqual(job.status, JobStatus.PENDING)
+        self.assertEqual(job.delay, 0)
+
+    def test_job_execute(self):
+        job = Job("job-1", lambda x: x + 1, 10)
+        self.assertEqual(job.job_id, "job-1")
+        self.assertEqual(job.status, JobStatus.PENDING)
+        self.assertEqual(job.delay, 0)
+        result = job.execute()
+        self.assertTrue(isinstance(result, JobResult))
+        self.assertEqual(result.value, 11)
+
+    def test_job_add_delay(self):
+        job = Job("job-1", lambda x: x + 1, 10)
+        self.assertEqual(job.job_id, "job-1")
+        self.assertEqual(job.status, JobStatus.PENDING)
+        self.assertEqual(job.delay, 0)
+        job.add_delay(2)
+        t1 = time.time()
+        result = job.execute()
+        t2 = time.time()
+        self.assertTrue(isinstance(result, JobResult))
+        self.assertEqual(result.value, 11)
+        self.assertAlmostEqual(t2 - t1, 2, delta=0.1)
