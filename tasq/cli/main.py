d94158d168236a7c58eee87e1fd2bdbd82588717
Andrea <a.g.baldan@gmail.com>
2020-07-17 18:02:33 +0200

    Updating CLI options

--- tasq/cli/main.py
+++ tasq/cli/main.py
@@ -29,18 +29,16 @@ runners = {
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Tasq CLI")
-    parser.add_argument("subcommand")
     parser.add_argument(
-        "--conf",
-        "-c",
-        help="The filepath to the configuration file, in json",
-        nargs="?",
+        "runner_type", choices=["runner", "redis-runner", "rabbitmq-runner"]
+    )
+    parser.add_argument(
+        "--conf", "-c", help="The filepath to the configuration file, in json",
     )
     parser.add_argument(
         "--address",
         "-a",
         help="The ZMQ host address to connect to, default to localhost",
-        nargs="?",
     )
     parser.add_argument(
         "--port",
@@ -48,37 +46,28 @@ def parse_arguments():
         help="The ZMQ port to connect to, default to 9000 for "
         "ZMQ/TCP/UNIX connections, to 6379 while using a "
         "redis broker or 5672 in case of RabbitMQ as backend.",
-        nargs="?",
         type=int,
     )
     parser.add_argument(
-        "--plport",
+        "--pull-port",
         help="The ZMQ port to connect to, default to 9001 for "
         "ZMQ/TCP/UNIX connections",
-        nargs="?",
         type=int,
     )
     parser.add_argument(
-        "--worker-type",
-        help="The type of worker to deploy for a runner",
-        nargs="?",
+        "--worker-type", help="The type of worker to deploy for a runner"
     )
     parser.add_argument(
-        "--db",
-        help="The database to use with redis as backend",
-        nargs="?",
-        type=int,
+        "--db", help="The database to use with redis as backend", type=int,
     )
     parser.add_argument(
         "--name",
         help="The name of the queue, only for redis or rabbitmq backends",
-        nargs="?",
     )
     parser.add_argument(
         "--signkey",
         help="The shared key to use to sign byte streams between clients and "
         "runners",
-        nargs="?",
     )
     parser.add_argument(
         "--unix",
@@ -90,10 +79,9 @@ def parse_arguments():
     parser.add_argument(
         "--num-workers",
         help="Number of workers to instantiate on the node",
-        nargs="?",
         type=int,
     )
-    parser.add_argument("--log-level", help="Set logging level", nargs="?")
+    parser.add_argument("--log-level", help="Set logging level")
     args = parser.parse_args()
     return args
 
@@ -113,14 +101,15 @@ def start_worker(runner_type, worker_type, host, **kwargs):
 def main():
     args = parse_arguments()
     conf = get_config(args.conf)
+    runner_type = args.runner_type or "runner"
     logger.loglevel = args.log_level or conf["log_level"]
     signkey = args.signkey or conf["signkey"]
     unix = conf["unix"]
     num_workers = args.num_workers or conf["num_workers"]
     worker_type = args.worker_type or "actor"
     addr = args.address or conf["addr"]
-    if args.subcommand == "worker":
-        push_port = args.plport or conf["zmq"]["push_port"]
+    if runner_type == "runner":
+        push_port = args.pull_port or conf["zmq"]["push_port"]
         pull_port = args.port or conf["zmq"]["pull_port"]
         start_worker(
             "zmq",
@@ -131,7 +120,7 @@ def main():
             num_workers=num_workers,
             unix=unix,
         )
-    elif args.subcommand == "redis-worker":
+    elif runner_type == "redis-runner":
         port = args.port or conf["redis"]["port"]
         db = args.db or conf["redis"]["db"]
         name = args.name or conf["redis"]["name"]
@@ -145,7 +134,7 @@ def main():
             num_workers=num_workers,
             signkey=signkey,
         )
-    elif args.subcommand == "rabbitmq-worker":
+    elif runner_type == "rabbitmq-runner":
         port = args.port or conf["rabbitmq"]["port"]
         name = args.name or conf["rabbitmq"]["name"]
         start_worker(
